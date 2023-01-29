a4ea72b660f163e52284b449430d079a4b647474
jamitrov <jana.mitrovska@netcetera.com>
2023-01-29 11:39:08 +0100

    [no-issue] Parallel tests are successful

--- src/test/java/utilities/DesiredCapabilitiesUtil.java
+++ src/test/java/utilities/DesiredCapabilitiesUtil.java
@@ -9,7 +9,6 @@ public class DesiredCapabilitiesUtil {
         desiredCapabilities.setCapability("platformVersion", platformVersion);
         desiredCapabilities.setCapability("deviceName", deviceName);
         desiredCapabilities.setCapability("udid", udid);
-        //desiredCapabilities.setCapability("wdaLocalPort",wda);
         desiredCapabilities.setCapability("skipUnlock", "true");
         desiredCapabilities.setCapability("noReset", "false");
 
@@ -18,13 +17,8 @@ public class DesiredCapabilitiesUtil {
             desiredCapabilities.setCapability("bundleId", "com.apple.MobileAddressBook");
             desiredCapabilities.setCapability("xcodeOrgId", "jana.mitrovska@gmail.com");
             desiredCapabilities.setCapability("xcodeSigningId", "Apple Development");
-            desiredCapabilities.setCapability("useNewWDA",true);
-            if(deviceName.equals("iPhone 12 Pro")){
-                desiredCapabilities.setCapability("wdaLocalPort", "8100");
-            }else if(deviceName.equals("iPhone 14 Pro")){
-                desiredCapabilities.setCapability("wdaLocalPort", "8200");
-            }
         } else if(platformName.equals("Android")){
+            desiredCapabilities.setCapability("skipUnlock", true);
             desiredCapabilities.setCapability("appPackage", "com.android.contacts");
             desiredCapabilities.setCapability("appActivity", "com.android.contacts.activities.PeopleActivity");
         }
